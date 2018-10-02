e6816b9e1614c8ce0440286d300af37ad9c08a7e
student <student@student-8.local>
2018-10-02 15:23:21 +0300

    Initial Commit

---
+++ GusarovvDaniil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GusarovvDaniil
+//
+//  Created by student on 02.10.2018.
+//  Copyright © 2018 Allola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
